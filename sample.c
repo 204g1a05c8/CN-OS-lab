0bf15ba5168943135ebbcdf0f8b95ca070160be8
204g1a05c8 <112164537+204g1a05c8@users.noreply.github.com>
2022-09-02 15:40:29 +0530

    p.sowmitha 204g1a05c8
    My sample.c file is created

---
+++ sample.c
@@ -0,0 +1,13 @@
+*/
+Name:P.Sowmitha
+Roll.no:204g1a05c8
+Title of the experiment:sample program
+Date of creation:02-9-2002
+Date of Execution:02-9-2002
+*/
+// source code
+#include <stdio.h>
+int main()
+{
+printf("HELLO WORLD\n")
+}
