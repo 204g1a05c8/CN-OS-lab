04c316187267d0e7503e0e5379f225e96852c48c
204g1a05c8 <112164537+204g1a05c8@users.noreply.github.com>
2022-09-16 16:06:24 +0530

    p.sowmitha created SJF.c

---
+++ oslab/exp2/SJF.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: p.sowmitha
+   Roll No: 204G1A05c8
+   Title of the Experiment: CPU Scheduling Using SJF Algorithm
+   Date of Creation: 16-09-2022
+   Date of Execution: 16-09-2022
+*/
